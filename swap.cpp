7ff87b52b5b02b834bb5260502792b3326a33447
shravanib2807-sys <shravanib2807@gmail.com>
2026-07-17 13:07:14 +0530

    Add files via upload

---
+++ swap.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,c;
+cout<<"Enter first number=";
+cin>>a;
+cout<<"Enter second number=";
+cin>>b;
+c=a;
+a=b;
+b=c;
+cout<<"a="<<a<<endl;
+cout<<"b="<<b<<endl;
+return 0;
+}
