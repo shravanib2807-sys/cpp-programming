f9a1c3f03a38d74937f36fc870b1e14806da066a
shravanib2807-sys <shravanib2807@gmail.com>
2026-07-24 07:04:03 +0000

    add files

---
+++ if-else.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter first number=";
+    cin>>a;
+    cout<<"enter second number=";
+    cin>>b;
+    cout<<"enter third number=";
+    cin>>c;
+    if(a>=b && a>=c){
+        cout<<a<<" is greatest number"<<endl;
+    }else if(b>=a && b>=c){
+        cout<<b<<" is greatest number"<<endl;
+    }else{
+        cout<<c<<" is greatest number"<<endl;
+    }
+    return 0;
+}
+    
+
