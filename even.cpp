d3e2790ee66f5fdb32d471125c6f6cd97f393c39
shravanib2807-sys <shravanib2807@gmail.com>
2026-07-24 06:36:27 +0000

    change

---
+++ even.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"number";
+    cin>>num;
+    if(num%2==0){
+        cout<<num<<"number is even"<<endl;
+    }else{
+        cout<<num<<"number is odd"<<endl;
+    }
+    return 0;
+}
