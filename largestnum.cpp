aade2468d83310c8274b7e8c39db39a634f51854
shravanib2807-sys <shravanib2807@gmail.com>
2026-07-24 06:47:35 +0000

    change

---
+++ largestnum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter first number=";
+    cin>>a;
+    cout<<"enter second number=";
+    cin>>b;
+    if(a>b){
+        cout<<a<<" is a largest number"<<endl;
+    }else{
+        cout<<b<<" is a largest number"<<endl;
+    }
+    return 0;
+}
