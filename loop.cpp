77dbe9947b195bd8201fa77702049ab9cdc010c9
shravanib2807-sys <shravanib2807@gmail.com>
2026-07-26 15:42:20 +0000

    change

--- loop.cpp
+++ loop.cpp
@@ -6,6 +6,6 @@ int main()
     cout<<"Enter the number=";
     cin>>num;
     for(int i=1;i<=10;i++){
-        cout<<num<<" x "<<i<<" = "<<num*i<<endl;
+        cout<<num<<" x "<<i<<" = "<<num*i<<endl; 
     }
 }
