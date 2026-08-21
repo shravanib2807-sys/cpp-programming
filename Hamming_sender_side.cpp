70717eaa701dfc36002079a8a3816a1bf58b0f63
shravanib2807-sys <shravanib2807@gmail.com>
2026-08-21 01:07:19 +0000

    add files

--- Hamming_sender_side.cpp
+++ Hamming_sender_side.cpp
@@ -7,18 +7,12 @@ int main()
     string data;
     cout << "Enter Data Word: ";
     cin >> data;
-
     int m = data.length();
     int r = 0;
-
-    // Find redundant bits
     while ((1 << r) < m + r + 1)
         r++;
-
     int n = m + r;
     int code[20] = {0};
-
-    // Put data bits
     int j = 0;
     for (int i = n; i >= 1; i--)
     {
@@ -28,8 +22,6 @@ int main()
             j++;
         }
     }
-
-    // Calculate parity bits
     for (int p = 1; p <= n; p = p * 2)
     {
         int parity = 0;
@@ -42,21 +34,15 @@ int main()
 
         code[p] = parity;
     }
-
-    // Display m, n and r
     cout << "\nm = " << m << endl;
     cout << "r = " << r << endl;
     cout << "n = " << n << endl;
-
-    // Display redundant bits
     cout << "\nRedundant Bits:\n";
 
     for (int p = 1; p <= n; p = p * 2)
     {
         cout << "R" << p << " = " << code[p] << endl;
     }
-
-    // Display code word
     cout << "\nCode Word = ";
 
     for (int i = n; i >= 1; i--)
