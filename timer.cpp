72736a0a0b6ae3a4afe18e7529380983b54966ef
shravanib2807-sys <shravanib2807@gmail.com>
2026-08-21 00:27:37 +0000

    add files

---
+++ timer.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class Time
+{
+    public:
+    int h1,m1,s1;
+    int h2,m2,s2;
+    void get()
+    {
+        cout<<"Enter first time";
+        cin>>h1>>m1>>s1;
+        cout<<"Enter second time";
+        cin>>h2>>m2>>s2;
+    }
+    void add()
+    {
+        int h,m,s;
+        s=s1+s2;
+        m=m1+m2+s/60;
+        s=s%60;
+        h=h1+h2+m/60;
+        m=m%60;
+        cout<<"Addition= "<<h<<"hours"<<m<<"minutes"<<s<<"seconds"<<endl;
+    }
+    void sub()
+    {
+        int h,m,s;
+        h=h1-h2;
+        m=m1-m2;
+        s=s1-s2;
+        if(s<0)
+        {
+            s=s+60;
+            m=m-1;
+        }
+        if(m<0)
+        {
+            m=m+60;
+            h=h-1;
+        }
+        cout<<"Substraction= "<<h<<"hours"<<m<<"minutes"<<s<<"seconds"<<endl;
+    }
+};
+int main(){
+    Time t;
+    t.get();
+    t.add();
+    t.sub();
+    return 0;
+}
